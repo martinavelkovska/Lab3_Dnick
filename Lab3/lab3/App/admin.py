7d3fda1c9d2894cdfc9ebcb4104320bc0acb8cfa
Martina Velkovska <103152926+martinavelkovska@users.noreply.github.com>
2023-06-06 04:18:44 +0200

    3

---
+++ Lab3/lab3/App/admin.py
@@ -0,0 +1,155 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Avtor, Komentar, Post, Blok
+from django.core.exceptions import PermissionDenied
+# Register your models here.
+
+class AvtorAdmin(admin.ModelAdmin):
+    list_display = ("ime", "prezime")
+
+
+    def has_change_permission(self, request, obj=None):
+        if obj and (request.user == obj.user):
+            return True
+        else:
+            return False
+
+    def has_view_permission(self, request, obj=None) -> bool:
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        if (obj and (request.user == obj.user)) or request.user.is_superuser:
+            return True
+        else:
+            return False
+
+    def has_add_permission(self, request, obj=None):
+        if(request.user.is_superuser):
+            return True
+
+    #def save_model(self, request, obj, form, change):
+        #if obj is not None:
+         #   obj.user = request.user
+
+        #super().save_model(request, obj, form, change)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("naslov", "avtor",)
+    search_fields = ("naslov", "sodrzina",)
+    list_filter = ("datum_created",)
+    exclude = ("avtor",)
+
+    def has_view_permission(self, request, obj=None) -> bool:
+        if obj is not None:
+            if request.user.is_superuser:
+                return True
+            #if obj.blocked_users.filter(id=request.user.id).exists():
+             #   raise PermissionDenied("You are not allowed to view this post.")
+            #    return False
+        return True
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser:
+            blocked = Blok.objects.filter(blokiran__user=request.user).values_list('bloker__user', flat=True)
+            qs = qs.exclude(avtor__user__in=blocked)
+        return qs
+
+    def has_change_permission(self, request, obj=None):
+        if (obj and request.user == obj.avtor.user) or request.user.is_superuser:
+            return True
+        else:
+            return False
+
+    def has_delete_permission(self, request, obj=None):
+        if (obj and request.user == obj.avtor.user ) or request.user.is_superuser:
+            return True
+        else:
+            return False
+
+    def has_add_permission(self, request, obj=None) -> bool:
+        return True
+
+    def save_model(self, request, obj, form, change):
+        if obj is not None:
+            if request.user.is_superuser:
+                obj.avtor = Avtor.objects.get(user=request.user)
+            else:
+                obj.avtor = Avtor.objects.get(user=request.user)
+
+
+        super().save_model(request, obj, form, change)
+admin.site.register(Post, PostAdmin)
+
+class KomentarAdmin(admin.ModelAdmin):
+    list_display = ("sodrzina", "datum_created" ,"user")
+    exclude = ("user",)
+
+    def has_view_permission(self, request, obj=None):
+        return request.user.is_superuser or (obj and Blok.objects.filter(bloker__user=obj.post.avtor.user).exists()) or request.user
+
+    def has_delete_permission(self, request, obj=None):
+        if obj and (request.user == obj.user.user or request.user == obj.post.avtor.user or request.user.is_superuser):
+            return True
+        else:
+            return False
+
+    def has_change_permission(self, request, obj=None):
+        if(obj and request.user == obj.user.user) or request.user.is_superuser:
+            return True
+        else:
+            return False
+
+
+    def has_add_permission(self, request) -> bool:
+        return True
+
+
+    def save_model(self, request, obj, form, change):
+        if obj is not None:
+            if request.user.is_superuser:
+                obj.user = Avtor.objects.get(user=request.user)
+               # obj.user.user = request.user
+            else:
+                obj.user = Avtor.objects.get(user=request.user)
+
+        super().save_model(request, obj, form, change)
+
+class BlokAdmin(admin.ModelAdmin):
+     exclude = ("bloker",)
+
+     def has_view_permission(self, request, obj=None):
+        if obj and (request.user == obj.bloker.user or request.user.is_superuser):
+            return True
+        return False
+
+     def has_change_permission(self, request, obj=None):
+        if obj and (request.user == obj.bloker.user or request.user.is_superuser):
+            return True
+        return False
+
+     def has_add_permission(self, request,  obj=None):
+        return True
+
+     def has_delete_permission(self, request, obj=None):
+        if obj and (request.user == obj.bloker.user or request.user.is_superuser):
+            return True
+        return False
+
+     def save_model(self, request, obj, form, change):
+        if obj is not None:
+             if request.user.is_superuser:
+                obj.bloker = Avtor.objects.get(user=request.user)
+             else:
+                obj.bloker = Avtor.objects.get(user=request.user)
+
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Avtor, AvtorAdmin)
+
+admin.site.register(Komentar, KomentarAdmin)
+
+admin.site.register(Blok, BlokAdmin)
---
+++ Lab3/lab3/App/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Avtor(models.Model):
+    ime = models.CharField(max_length=50)
+    prezime = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.ime+" "+self.prezime
+
+class Post(models.Model):
+    naslov = models.CharField(max_length=50)
+    avtor = models.ForeignKey(Avtor, on_delete=models.CASCADE)
+    sodrzina = models.TextField(null=True, blank=True)
+    datum_created = models.DateField(auto_now_add=True)
+    datum_last_modified = models.DateField(auto_now_add=True)
+   # blocked_users = models.ManyToManyField(User, related_name='blocked_posts', blank=True)
+    file = models.FileField(upload_to="files/", null=True, blank=True)
+
+    def __str__(self):
+        return self.naslov
+
+class Komentar(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(Avtor, on_delete=models.CASCADE)
+    sodrzina = models.TextField(null=True, blank=True)
+    datum_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.sodrzina
+
+class Blok(models.Model):
+   bloker = models.ForeignKey(Avtor, on_delete=models.CASCADE, related_name="user_bloker")
+   blokiran = models.ForeignKey(Avtor, on_delete=models.CASCADE, related_name="user_blokiran")
+  # blokirani_posts = models.ManyToManyField(Post, blank=True)
+
+   def __str__(self):
+        return str(self.bloker) + " blocked " + str(self.blokiran)
